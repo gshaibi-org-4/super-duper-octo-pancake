5a00ea7ac62e341b3926d8879f28cb2c3a421b96
gshaibi <gshaibi@nvidia.com>
2026-03-04 16:31:06 +0200

    .

--- pkg/file.go
+++ pkg/file.go
@@ -7,3 +7,15 @@ func AddOne(n int) int {
 func AddTwo(n int) int {
 	return n + 2
 }
+
+func AddThree(n int) int {
+	return n + 3
+}
+
+func AddFour(n int) int {
+	return n + 4
+}
+
+func AddFive(n int) int {
+	return n + 5
+}
